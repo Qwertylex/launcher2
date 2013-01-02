050d076b6321df0e783eb6a6e6c11b1a51c32a97
Zarek Jenkinson <me@akiwiguy.net>
2013-01-02 20:37:35 +1300

    archiving fix thing in make script

--- make.bat
+++ make.bat
@@ -36,7 +36,7 @@ goto :eof
 
 :package
     echo [::] Creating package
-    cd launcher2\bin\Release\%BUILDPLATFORM%
+    cd launcher2\bin\%BUILDCONFIG%\%BUILDPLATFORM%
     "C:\Program Files\7-Zip\7z.exe" a -y ..\..\..\launcher2-release.7z *.exe *.dll ..\..\..\..\buildinfo.txt
     cd ..\..\..\..
     goto :eof
