ea3463fa3fe99cdd78f17a433f6c34b211af948c
Zarek Jenkinson <me@akiwiguy.net>
2013-01-02 16:38:10 +1300

    Vastly improved make script
    Supports specifying build target/platform, generating a build info file
    that is included in the package by default (includes git commit hash,
    build date/time, build host info, and build configuration) and cleaned
    up the layout

--- make.bat
+++ make.bat
@@ -1,29 +1,54 @@
 @echo off
 
-if "%~1"=="" goto :all
-for %%A in (%*) do ( call :%%A )
+if not defined BUILDCONFIG set BUILDCONFIG=Release
+if not defined BUILDPLATFORM set BUILDPLATFORM=x86
+
+if "%~1"=="" ( 
+    goto :all
+) else (
+    for %%A in (%*) do (
+        set BUILDFUNC=%%A
+        call :%%A
+    )
+)
+
 goto :eof
 
 :all
     call :clean
-    call :release
+    call :build
+    call :package
     goto :eof
 
 :clean
     echo [::] Cleaning
-    del /S /Q launcher2\bin launcher2\obj *.cache launcher2-release.7z
+    del /S /Q launcher2\bin launcher2\obj *.cache launcher2-release.7z buildinfo.txt
     goto :eof
 
-:debug
-    echo [::] Building debug
-    C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe launcher2.sln /t:Rebuild /p:Configuration=Debug
+:build
+    echo [::] Building launcher2 for %BUILDCONFIG% platform %BUILDPLATFORM%
+    call :buildid
+    C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe launcher2.sln /t:Rebuild /p:Configuration="%BUILDCONFIG%" /p:Platform="%BUILDPLATFORM%"
     goto :eof
 
-:release
-    echo [::] Building release
-    C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe launcher2.sln /t:Rebuild /p:Configuration=Release
-    echo [::] Packaging release
+:package
+    echo [::] Creating package
     cd launcher2\bin\Release
-    "C:\Program Files\7-Zip\7z.exe" a -y ..\..\..\launcher2-release.7z *.exe *.dll
+    "C:\Program Files\7-Zip\7z.exe" a -y ..\..\..\launcher2-release.7z *.exe *.dll ..\..\..\buildinfo.txt
     cd ..\..\..
+    goto :eof
+
+:buildid
+    if exist ".git/refs/heads/master" ( set /p GITCOMMIT=<".git/refs/heads/master" ) else ( set /p GITCOMMIT="unknown" )
+    for /f "tokens=2,3,4 delims=[.]" %%a in ('ver') do set WINVER=%%a.%%b.%%c
+    echo launcher2 build information > buildinfo.txt
+    echo --------------------------- >> buildinfo.txt
+    echo Build host user: %COMPUTERNAME%\%USERNAME% >> buildinfo.txt
+    echo Build host OS: %WINVER% >> buildinfo.txt
+    echo Build host architecture: %PROCESSOR_ARCHITECTURE% >> buildinfo.txt
+    echo Build Git commit: %GITCOMMIT% >> buildinfo.txt
+    echo Build time: %date% %time% >> buildinfo.txt
+    echo Build configuration: %BUILDCONFIG% >> buildinfo.txt
+    echo Build platform: %BUILDPLATFORM% >> buildinfo.txt
+    echo --------------------------- >> buildinfo.txt
     goto :eof
