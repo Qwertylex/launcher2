e1fdb1214d4d7d06697f0254a7cd087b641a7cdf
Zarek Jenkinson <me@akiwiguy.net>
2013-01-10 00:03:13 +1300

    huge comments in updater thing
    why? why not? i don't think there's comments *anywhere else* in
    launcher2 so might as well start somewhere

--- launcher2/MinecraftUpdater.cs
+++ launcher2/MinecraftUpdater.cs
@@ -8,9 +8,41 @@ using System.IO;
 
 public class MinecraftUpdater {
 	public MinecraftUpdater() {
+        /*
+        do we actually need to do anything here? at the moment, no
+        but if i end up caching the XML from assets.minecraft.net, here's where to do it.
+        */
+
 		Debug.WriteLine("[MinecraftUpdater] New MinecraftUpdater created");
     }
 
+    /*
+    Quick and dirty rundown of GetAvailableVersions()
+    -------------------------------------------------
+    Here's the basics of how the XML we get from assets.minecraft.net is structured:
+
+        <ListBucketResult>
+            <Name>assets.minecraft.net</Name>
+            ...
+            <Contents>
+                <Key>11w47a/minecraft.jar</Key>
+                ...
+            </Contents>
+            <Contents>
+                ...
+            </Contents>
+            ...
+        </ListBucketResult>
+
+    We just loop through looking for <Key> elements, grabbing their values, checking if they
+    have "minecraft.jar" in them, and if they do, shoving them in a List<string> and returning
+    that when we've iterated through the entire XML.
+
+    We also do some really dirty string replacement so that what we return isn't filled with 
+    underscores just so it looks nice when displayed in the user interface. (yes, that should
+    have been done in the UI, shut up. it was easier to do here, and I'm **horribly** lazy)
+    */
+    
     public List<string> GetAvailableVersions() {
         List<string> _collection = new List<string>();
     	WebClient webClient = new WebClient();
